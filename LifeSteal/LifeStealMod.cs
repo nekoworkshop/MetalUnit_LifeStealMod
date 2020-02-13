0a97157e41e344a751c8626e7de63790aa83d796
Nekoworkshop <nekoworkshop@gmail.com>
2020-02-12 22:26:40 -0500

    Add project files.

---
+++ LifeSteal/LifeStealMod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Harmony12;
+using UnityModManagerNet;
+
+namespace LifeSteal
+{
+    public class LifeStealMod
+    {
+    }
+}
